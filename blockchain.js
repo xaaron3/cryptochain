aa27246c4d661ce0bc8b50fe3f6b8f8801005fd2
aaron <xakong@outlook.com>
2019-02-14 18:18:12 -0500

    pow, difficulty and nonce value tests / code, dynamic difficulty and mine rate, adjust diff in mineblock, improve pow system, avg work script

--- blockchain.js
+++ blockchain.js
@@ -36,13 +36,13 @@ class Blockchain {
       }
 
       for (let i = 1; i < chain.length; i++) {
-         const { timestamp, lastHash, hash, data } = chain[i]; // destruc frm block
+         const { timestamp, lastHash, hash, nonce, difficulty, data } = chain[i]; // destruc frm block
 
          const actualLastHash = chain[i - 1].hash;
 
          if (lastHash !== actualLastHash) return false;
 
-         const validatedHash = cryptoHash(timestamp, lastHash, data);
+         const validatedHash = cryptoHash(timestamp, lastHash, data, nonce, difficulty);
 
          if (hash !== validatedHash) return false;
       }
