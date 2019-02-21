cff1673c32c2d41b329835feae1d916dd73959c8
aaron <xakong@outlook.com>
2019-02-21 15:17:50 -0500

    validate transaction

--- wallet/index.test.js
+++ wallet/index.test.js
@@ -1,4 +1,5 @@
 const Wallet = require('./index');
+const { verifySignature } = require('../util');
 
 describe('Wallet', () => {
    let wallet;
@@ -14,4 +15,28 @@ describe('Wallet', () => {
    it('has a `publicKey`', () => {
       expect(wallet).toHaveProperty('publicKey');
    });
-});
+
+   describe('signing data', () => {
+      const data = 'foobar';
+
+      it('verifies a signature', () => {
+         expect(
+            verifySignature({
+               publicKey: wallet.publicKey, 
+               data,
+               signature: wallet.sign(data)
+            })
+         ).toBe(true);
+      });
+
+      it('does not very an invalid signature', () => {
+         expect(
+            verifySignature({
+               publicKey: wallet.publicKey,
+               data,
+               signature: new Wallet().sign(data)
+            })
+         ).toBe(false);
+      });
+   });
+}); 
